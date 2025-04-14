c85b2604829b66aaef6c935f60d2406b4f221b7d
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-04-14 20:57:14 +0300

    feat: implement a solution for logger, partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -192,8 +192,17 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    const funcName = func.name || 'anonymous';
+
+    logFunc(`${funcName}(${argStr}) starts`);
+    const result = func(...args);
+    logFunc(`${funcName}(${argStr}) ends`);
+
+    return result;
+  };
 }
 
 /**
@@ -209,8 +218,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
